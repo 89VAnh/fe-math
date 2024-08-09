6fe83d5e9f7917c54b62b88d89e3f73ba3d0ef6a
89VAnh <vietanha9a2@gmail.com>
2024-08-09 08:35:52 +0700

    update

--- src/app/(default-layout)/test/page.tsx
+++ src/app/(default-layout)/test/page.tsx
@@ -1,25 +1,50 @@
 "use client";
 import Search from "@/components/Search";
+import { useSearchLevel } from "@/helper/data/level.loader";
 import { useSearchTest } from "@/helper/data/test.loader";
+import { Level } from "@/types/Level";
 import { Test } from "@/types/Test";
 import { BASE_URL } from "@/utils/config";
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Key, useState } from "react";
 
 export default function Page() {
   const [page, setPage] = useState(1);
   const [searchContent, setSearchContent] = useState("");
   const PAGE_SIZE = 10;
   const router = useRouter();
+  const [level, setLevel] = useState<Key>();
 
+  const { data: levels } = useSearchLevel({});
   const { data, isLoading } = useSearchTest({
     page,
     pageSize: PAGE_SIZE,
     searchContent,
+    level,
   });
   return (
     <div className='rounded-[10px] border border-stroke bg-white p-4 shadow-1 dark:border-dark-3 dark:bg-gray-dark dark:shadow-card sm:p-7.5'>
+      {levels?.data && (
+        <Tabs
+          aria-label='Tabs radius'
+          classNames={{
+            base: "w-full my-4",
+            tabList: "w-full",
+          }}
+          onSelectionChange={(key: Key) => setLevel(key)}>
+          {levels?.data.map((level: Level) => (
+            <Tab key={level?.id} title={level?.name} />
+          ))}
+        </Tabs>
+      )}
       <Search
         handleSearch={(formData) =>
           setSearchContent(formData.get("search")?.toString() || "")
